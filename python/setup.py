208dafc4e8f8aa11851a681f155ecfd2282f787a
Adam Wierzbicki <awierzbicki@golem.network>
2019-09-13 12:28:49 +0200

    Bump package to v0.18.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Golem-Task-Api',
-    version='0.17.0',
+    version='0.18.0',
     url='https://github.com/golemfactory/golem/task-api/python',
     maintainer='The Golem team',
     maintainer_email='tech@golem.network',
