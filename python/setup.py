09dbe0282e752aef0bf9f94f158931b6e8fe9f4e
Adam Wierzbicki <awierzbicki@golem.network>
2019-09-16 11:08:29 +0200

    [python] Fixed typing bug (runnning) (#47)

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Golem-Task-Api',
-    version='0.18.1',
+    version='0.18.2',
     url='https://github.com/golemfactory/golem/task-api/python',
     maintainer='The Golem team',
     maintainer_email='tech@golem.network',
