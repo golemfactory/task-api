cd6d8f7ec457fb71ea406e6e386802821ef141ce
Marek Franciszkiewicz <marek@golem.network>
2019-09-16 17:40:05 +0200

    New: generate enums from nested types

--- python/gen_enums.py
+++ python/gen_enums.py
@@ -6,27 +6,33 @@ from google.protobuf.descriptor import EnumDescriptor
 
 def main(input_module_file: Path, output_file: Path):
     src = """# This file is auto-generated from gen_enums.py
-from enum import Enum
-"""
+from enum import Enum"""
 
     input_module_src = input_module_file.read_text('utf-8')
     input_module = dict()
     exec(input_module_src, input_module)
 
     def enum_src(enum_name: str, enum_descriptor: EnumDescriptor) -> str:
-        code = f"\n\nclass {enum_name}(Enum):"
+        code = f"\n\n\nclass {enum_name}(Enum):"
         for value_descriptor in list(enum_descriptor.values):
             code += f"\n    {value_descriptor.name} = {value_descriptor.number}"
         return code
 
-    # Top level enums
-    for name, desc in input_module['DESCRIPTOR'].enum_types_by_name.items():
-        src += enum_src(name, desc)
+    def parse_enums(descriptor) -> str:
+        code = ''
+        for enum_name, enum_desc in descriptor.enum_types_by_name.items():
+            code += enum_src(enum_name, enum_desc)
+        return code
 
-    # Enums embedded in messages
-    for _, msg_desc in input_module['DESCRIPTOR'].message_types_by_name.items():
-        for name, desc in msg_desc.enum_types_by_name.items():
-            src += enum_src(name, desc)
+    def parse_message(descriptor) -> str:
+        code = parse_enums(descriptor)
+        for _, type_desc in descriptor.nested_types_by_name.items():
+            code += parse_message(type_desc)
+        return code
+
+    src += parse_enums(input_module['DESCRIPTOR'])
+    for _, desc in input_module['DESCRIPTOR'].message_types_by_name.items():
+        src += parse_message(desc)
 
     with open(output_file, 'w') as f:
         f.write(src)
