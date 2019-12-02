9d9b3f78d75a20e8f18817eb2f3860ed6f8604fe
Marek Franciszkiewicz <mfranciszkiewicz@users.noreply.github.com>
2019-12-02 17:22:56 +0100

    DockerTaskAPIService and process utils (#59)

---
+++ python/golem_task_api/apputils/process.py
@@ -0,0 +1,52 @@
+import asyncio
+import time
+
+from dataclasses import dataclass
+from golem_task_api.threading import Executor
+from psutil import Process
+
+
+@dataclass
+class Usage:
+    mem_peak: float = 0.  # bytes
+    cpu_time: float = 0.
+    real_time: float = 0.
+
+
+def _monitor_pid(
+        pid: int,
+        usage: Usage,
+        interval: float = 0.5
+) -> None:
+    proc = Process(pid)
+    while proc.is_running() and not Executor.is_shutting_down():
+        usage.cpu_time = sum(proc.cpu_times())
+        usage.mem_peak = max(usage.mem_peak, proc.memory_info().vms)
+        time.sleep(interval)
+
+
+async def exec_and_monitor_cmd(cmd):
+    usage = Usage()
+    time_started = time.time()
+
+    process = await asyncio.create_subprocess_exec(*cmd)
+    future = Executor.run(_monitor_pid, process.pid, usage)
+    asyncio.ensure_future(future)
+
+    try:
+        return_code = await process.wait()
+    except asyncio.CancelledError:
+        process.terminate()
+        raise
+
+    usage.real_time = time.time() - time_started
+    return return_code, usage
+
+
+async def exec_cmd(cmd):
+    process = await asyncio.create_subprocess_exec(*cmd)
+    try:
+        return await process.wait()
+    except asyncio.CancelledError:
+        process.terminate()
+        raise
---
+++ python/golem_task_api/testutils/dockertaskapiservice.py
@@ -0,0 +1,129 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+from typing import Tuple, Optional
+
+import docker
+from golem_task_api import constants, TaskApiService
+
+
+def is_docker_available():
+    """ Check the connection with Docker daemon """
+    try:
+        client = docker.from_env()
+        client.ping()
+    except Exception:
+        return False
+    return True
+
+
+def run_docker_container(
+        image: str,
+        work_dir: Path,
+        command: str,
+        port: int
+) -> docker.models.containers.Container:
+    """" Start a detached container """
+    if sys.platform == 'linux':
+        ports = {}
+    else:
+        ports = {port: port}
+
+    client = docker.from_env()
+    return client.containers.run(
+        image=image,
+        command=command,
+        volumes={
+            str(work_dir): {
+                'bind': f'/{constants.WORK_DIR}',
+                'mode': 'rw',
+            }
+        },
+        user=os.getuid(),
+        ports=ports,
+        detach=True,
+    )
+
+
+def get_docker_container_port_mapping(
+        container_id: str,
+        port: int,
+        vm_name: str,
+) -> Tuple[str, int]:
+    """ Retrieve running container's IP address and port """
+
+    client = docker.APIClient()
+    container_config = client.inspect_container(container_id)
+    net_config = container_config['NetworkSettings']
+
+    if sys.platform == 'darwin':
+        ip_address = '127.0.0.1'
+    elif sys.platform == 'win32':
+        ip_address = subprocess.check_output(['docker-machine', 'ip', vm_name])
+        ip_address = ip_address.decode('utf-8').strip()
+    else:
+        ip_address = net_config['Networks']['bridge']['IPAddress']
+
+    if sys.platform != 'linux':
+        port = int(net_config['Ports'][f'{port}/tcp'][0]['HostPort'])
+
+    if not ip_address:
+        raise RuntimeError(f'Unable to read the IP address of {container_id}')
+
+    return ip_address, port
+
+
+class DockerTaskApiService(TaskApiService):
+
+    def __init__(
+            self,
+            image: str,
+            work_dir: Path,
+            vm_name: Optional[str] = 'default'
+    ) -> None:
+        self._image = image
+        self._work_dir = work_dir
+        self._vm_name = vm_name
+        self._container = None
+
+    async def start(
+            self,
+            command: str,
+            port: int
+    ) -> Tuple[str, int]:
+        self._container = run_docker_container(
+            self._image,
+            self._work_dir,
+            command,
+            port)
+
+        return get_docker_container_port_mapping(
+            self._container.id,
+            port,
+            self._vm_name)
+
+    async def stop(self) -> None:
+        if not self.running():
+            return
+        self._container.stop()
+
+    def running(self) -> bool:
+        try:
+            self._container.reload()
+        except (AttributeError, docker.errors.APIError):
+            return False
+        return self._container.status not in ['exited', 'error']
+
+    async def wait_until_shutdown_complete(self) -> None:
+        print(f'Shutting down container with status: {self._container.status}')
+        if not self.running():
+            return
+
+        logs = self._container.logs()
+        logs = logs.decode('utf-8')
+
+        try:
+            self._container.remove(force=True)
+        finally:
+            print(logs)
