1648de47650377ca94621e124350e9f3b4a07998
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-03-02 13:01:28 +0100

    Update version to 0.25 for golem freeze 0.23

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Golem-Task-Api',
-    version='0.24.4',
+    version='0.25.0',
     url='https://github.com/golemfactory/golem/task-api/python',
     maintainer='The Golem team',
     maintainer_email='tech@golem.network',
